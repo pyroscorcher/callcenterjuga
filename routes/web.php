318598cf6e3b8322c342075228e9f4179178a16c
pyroscorcher <pyroscorcherthariq@apps.ipb.ac.id>
2026-07-15 10:35:52 +0700

    endpoint chatbot berhasil

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\Api\WebhookController;
+use App\Http\Controllers\Api\WebhookController;
 
 Route::inertia('/', 'Welcome')->name('home');
+Route::post('/webhook/whatsapp', [WebhookController::class, 'terimaDariWhatsapp']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
 });
-
-Route::post('/webhook/whatsapp', [WebhookController::class, 'terimaDariWhatsapp']);
 
 require __DIR__.'/settings.php';
